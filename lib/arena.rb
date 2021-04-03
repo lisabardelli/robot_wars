4724c66571e4de178768bcc3949ad6cf2932bf4c
Lisa <bardelli.lisa@gmail.com>
2021-04-03 10:15:06 +0100

    chore: run rubocop

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,14 +1,13 @@
 class Arena
-    attr_reader :top_right_corner
+  attr_reader :top_right_corner
 
-    def initialize(top_right_corner)
-        @top_right_corner = top_right_corner
-    end 
+  def initialize(top_right_corner)
+    @top_right_corner = top_right_corner
+  end
 
-    def is_input_a_string(input)
-        raise 'Arena input must be a string' if !input.is_a? String
-    
-        true
-      end
-    
-end 
+  def input_a_string?(input)
+    raise 'Arena input must be a string' unless input.is_a? String
+
+    true
+  end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,10 @@
-require_relative "./arena.rb"
+require_relative './arena'
 
 def build_arena
-    puts 'Please enter the coordinates of the top right corner of the arena'
-    input_top_right_corner = gets.chomp
-    arena = Arena.new(input_top_right_corner)
-    puts arena.top_right_corner
-end 
+  puts 'Please enter the coordinates of the top right corner of the arena'
+  input_top_right_corner = gets.chomp
+  arena = Arena.new(input_top_right_corner)
+  puts arena.top_right_corner
+end
 
 build_arena
