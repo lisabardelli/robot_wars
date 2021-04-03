46efdb295400547bf958ed8487f74c5ddcb03fac
Lisa <bardelli.lisa@gmail.com>
2021-04-03 14:23:57 +0100

    feat: validate location coordinates (non negative)

--- lib/location.rb
+++ lib/location.rb
@@ -2,7 +2,14 @@ class Location
   attr_reader :x, :y
 
   def initialize(x, y)
+    check_value(x)
     @x = x
+    check_value(y)
     @y = y
   end
+
+
+  def check_value(value)
+    raise 'Negative value' if value < 0
+  end 
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -6,4 +6,9 @@ describe 'Location' do
     expect(location.x).to eq 5
     expect(location.y).to eq 5
   end
+  it 'returns an error if x or/and y is/are negative ' do
+    expect { Location.new(5, -5) }.to raise_error 'Negative value'
+    expect { Location.new(-5, 5) }.to raise_error 'Negative value'
+    expect { Location.new(-5, -5) }.to raise_error 'Negative value'
+  end
 end
